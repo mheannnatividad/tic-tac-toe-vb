ade2fa2c3fc713310cae7cc15d8a8b96aaef857e
mary ann <maryannbernardo212@gmail.com>
2022-05-28 00:19:17 +0800

    design interface

---
+++ tic-tac-toe-program/tic-tac-toe-program/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.clickButton7 = New System.Windows.Forms.Button()
+        Me.clickButton4 = New System.Windows.Forms.Button()
+        Me.clickButton8 = New System.Windows.Forms.Button()
+        Me.clickButton1 = New System.Windows.Forms.Button()
+        Me.clickButton5 = New System.Windows.Forms.Button()
+        Me.clickButton9 = New System.Windows.Forms.Button()
+        Me.clickButton6 = New System.Windows.Forms.Button()
+        Me.clickButton2 = New System.Windows.Forms.Button()
+        Me.clickButton3 = New System.Windows.Forms.Button()
+        Me.buttonReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'clickButton7
+        '
+        Me.clickButton7.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton7.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton7.Location = New System.Drawing.Point(13, 12)
+        Me.clickButton7.Name = "clickButton7"
+        Me.clickButton7.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton7.TabIndex = 0
+        Me.clickButton7.Text = "X"
+        Me.clickButton7.UseVisualStyleBackColor = False
+        '
+        'clickButton4
+        '
+        Me.clickButton4.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton4.Location = New System.Drawing.Point(13, 105)
+        Me.clickButton4.Name = "clickButton4"
+        Me.clickButton4.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton4.TabIndex = 0
+        Me.clickButton4.Text = "X"
+        Me.clickButton4.UseVisualStyleBackColor = False
+        '
+        'clickButton8
+        '
+        Me.clickButton8.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton8.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton8.Location = New System.Drawing.Point(143, 12)
+        Me.clickButton8.Name = "clickButton8"
+        Me.clickButton8.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton8.TabIndex = 0
+        Me.clickButton8.Text = "X"
+        Me.clickButton8.UseVisualStyleBackColor = False
+        '
+        'clickButton1
+        '
+        Me.clickButton1.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton1.Location = New System.Drawing.Point(13, 198)
+        Me.clickButton1.Name = "clickButton1"
+        Me.clickButton1.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton1.TabIndex = 0
+        Me.clickButton1.Text = "X"
+        Me.clickButton1.UseVisualStyleBackColor = False
+        '
+        'clickButton5
+        '
+        Me.clickButton5.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton5.Location = New System.Drawing.Point(143, 105)
+        Me.clickButton5.Name = "clickButton5"
+        Me.clickButton5.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton5.TabIndex = 0
+        Me.clickButton5.Text = "X"
+        Me.clickButton5.UseVisualStyleBackColor = False
+        '
+        'clickButton9
+        '
+        Me.clickButton9.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton9.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton9.Location = New System.Drawing.Point(273, 12)
+        Me.clickButton9.Name = "clickButton9"
+        Me.clickButton9.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton9.TabIndex = 0
+        Me.clickButton9.Text = "X"
+        Me.clickButton9.UseVisualStyleBackColor = False
+        '
+        'clickButton6
+        '
+        Me.clickButton6.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton6.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton6.Location = New System.Drawing.Point(273, 105)
+        Me.clickButton6.Name = "clickButton6"
+        Me.clickButton6.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton6.TabIndex = 0
+        Me.clickButton6.Text = "X"
+        Me.clickButton6.UseVisualStyleBackColor = False
+        '
+        'clickButton2
+        '
+        Me.clickButton2.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton2.Location = New System.Drawing.Point(143, 198)
+        Me.clickButton2.Name = "clickButton2"
+        Me.clickButton2.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton2.TabIndex = 0
+        Me.clickButton2.Text = "X"
+        Me.clickButton2.UseVisualStyleBackColor = False
+        '
+        'clickButton3
+        '
+        Me.clickButton3.BackColor = System.Drawing.Color.MistyRose
+        Me.clickButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clickButton3.Location = New System.Drawing.Point(273, 198)
+        Me.clickButton3.Name = "clickButton3"
+        Me.clickButton3.Size = New System.Drawing.Size(124, 87)
+        Me.clickButton3.TabIndex = 0
+        Me.clickButton3.Text = "X"
+        Me.clickButton3.UseVisualStyleBackColor = False
+        '
+        'buttonReset
+        '
+        Me.buttonReset.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.buttonReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonReset.Location = New System.Drawing.Point(12, 291)
+        Me.buttonReset.Name = "buttonReset"
+        Me.buttonReset.Size = New System.Drawing.Size(383, 50)
+        Me.buttonReset.TabIndex = 1
+        Me.buttonReset.Text = "Reset"
+        Me.buttonReset.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RosyBrown
+        Me.ClientSize = New System.Drawing.Size(412, 353)
+        Me.Controls.Add(Me.buttonReset)
+        Me.Controls.Add(Me.clickButton3)
+        Me.Controls.Add(Me.clickButton6)
+        Me.Controls.Add(Me.clickButton9)
+        Me.Controls.Add(Me.clickButton2)
+        Me.Controls.Add(Me.clickButton5)
+        Me.Controls.Add(Me.clickButton8)
+        Me.Controls.Add(Me.clickButton1)
+        Me.Controls.Add(Me.clickButton4)
+        Me.Controls.Add(Me.clickButton7)
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents clickButton7 As Button
+    Friend WithEvents clickButton4 As Button
+    Friend WithEvents clickButton8 As Button
+    Friend WithEvents clickButton1 As Button
+    Friend WithEvents clickButton5 As Button
+    Friend WithEvents clickButton9 As Button
+    Friend WithEvents clickButton6 As Button
+    Friend WithEvents clickButton2 As Button
+    Friend WithEvents clickButton3 As Button
+    Friend WithEvents buttonReset As Button
+End Class
